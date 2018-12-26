90245bd8f63d88398760fc503c184dd6de147e15
simonlee <simon_lee@gss.com.tw>
2018-12-26 12:07:19 +0800

    add HttpUtility

---
+++ DotNet_LibraryTutorial/SendHttpRequest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendHttpRequest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "192.168.1.1";
+            //test get method
+            var response = HttpUtility.HttpGet(uri);
+            Console.WriteLine($"Get Response: {response}");
+
+            //test post method
+            string parameters = string.Empty;
+            response = HttpUtility.HttpPost(uri, parameters);
+            Console.WriteLine($"Post Response: {response}");
+
+            Console.ReadKey();
+        }
+    }
+}
